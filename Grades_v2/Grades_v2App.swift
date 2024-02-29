197ec1b741f6e8b282c248216b6a4bb633d2584e
Jakob <jakob.erlebach@icloud.com>
2024-02-29 15:30:40 +0100

    Initial Commit

---
+++ Grades_v2/Grades_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Grades_v2App.swift
+//  Grades_v2
+//
+//  Created by Jakob Erlebach on 29.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Grades_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
